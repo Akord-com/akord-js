312b7ae6a2e7cf06a1230c14c5149a89947f16ce
Weronika K <weronika.kolodziejak@gmail.com>
2023-02-17 14:39:57 +0100

    feat: add stack import test case

--- src/__tests__/stack.test.ts
+++ src/__tests__/stack.test.ts
@@ -4,6 +4,7 @@ import { initInstance } from './helpers';
 import { email, password } from './data/test-credentials';
 import { NodeJs } from "../types/file";
 import { StorageType } from "../types/node";
+import { getTxData } from "../arweave";
 
 let akord: Akord;
 
@@ -103,5 +104,19 @@ describe("Testing stack functions", () => {
     await akord.stack.restore(stackId)
     const stack = await akord.stack.get(stackId);
     expect(stack.status).toEqual("ACTIVE");
+  });
+
+  it("should import new stack from arweave tx", async () => {
+    const fileTxId = "kzGxbFW_oJ3PyYneRs9cPrChQ-k-8Fym5k9PCZNJ_HA";
+    const fileName = fileTxId + ".jpeg";
+    const { stackId } = await akord.stack.import(vaultId, fileTxId);
+
+    const stack = await akord.stack.get(stackId);
+    expect(stack.name).toEqual(fileName);
+    expect(stack.versions.length).toEqual(1);
+    expect(stack.versions[0].name).toEqual(fileName);
+
+    const { data } = await akord.stack.getVersion(stackId);
+    expect(data).toEqual(await getTxData(fileTxId));
   });
 });
