aaeac4536978534fb3fdb678160311e5a8e999b0
jarrvis <michal.kaliszewski@pl.bosch.com>
2023-05-11 16:14:50 +0200

    feat: export types from root

---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export * from "./batch-response";
+export * from "./file"
+export * from "./membership";
+export * from "./node";
+export * from "./transaction";
+export * from "./user";
+export * from "./vault";
