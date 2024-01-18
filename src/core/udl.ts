e17a419a2b4d35dbd2f67b6d235acc68a98fd880
Weronika K <weronika.kolodziejak@gmail.com>
2024-01-18 17:14:47 +0100

    feat: update UDL to v2

--- src/core/udl.ts
+++ src/core/udl.ts
@@ -1,4 +1,4 @@
-import { CommercialUse, Derivation, LicenseFee, UDL, UDL_LICENSE_TX_ID, tagNames } from "../types/udl";
+import { CommercialUse, DataModelTraining, Derivation, LicenseFee, UDL, UDL_LICENSE_TX_ID, tagNames } from "../types/udl";
 import { Tag, Tags } from "../types/contract";
 import { BadRequest } from "../errors/bad-request";
 
@@ -6,7 +6,7 @@ export const udlToTags = (udl: UDL): Tags => {
   const tags = [] as Tags;
   tags.push(new Tag(tagNames.LICENSE, udl.license || UDL_LICENSE_TX_ID));
   if (udl.licenseFee) {
-    tags.push(new Tag(tagNames.LICENSE_FEE, setLicenseFee(udl.licenseFee)));
+    tags.push(new Tag(tagNames.ACCESS_FEE, setLicenseFee(udl.licenseFee)));
   }
   if (udl.commercialUses) {
     for (let commercialUse of udl.commercialUses) {
@@ -16,6 +16,11 @@ export const udlToTags = (udl: UDL): Tags => {
   if (udl.derivations) {
     for (let derivation of udl.derivations) {
       tags.push(new Tag(setDuration(derivation, tagNames.DERIVATION), setDerivation(derivation)));
+    }
+  }
+  if (udl.dataModelTrainings) {
+    for (let dataModelTraining of udl.dataModelTrainings) {
+      tags.push(new Tag(setDuration(dataModelTraining, tagNames.DATA_MODEL_TRAINING), setDataModelTraining(dataModelTraining)));
     }
   }
   if (udl.expires) {
@@ -29,6 +34,9 @@ export const udlToTags = (udl: UDL): Tags => {
   }
   if (udl.paymentAddress) {
     tags.push(new Tag(tagNames.PAYMENT_ADDRESS, udl.paymentAddress));
+  }
+  if (udl.unknownUsageRights && udl.unknownUsageRights === "Excluded") {
+    tags.push(new Tag(tagNames.UNKNOWN_USAGE_RIGHTS, "Excluded"));
   }
   return tags;
 }
@@ -45,13 +53,32 @@ export const setLicenseFee = (licenseFee: LicenseFee): string => {
   return licenseFee.type + "-" + licenseFee.value;
 }
 
-export const setDerivation = (derivation: Derivation): string => {
+export const setDataModelTraining = (dataModelTraining: DataModelTraining): string => {
+  let derivationValue = dataModelTraining.type as string;
+  if (dataModelTraining.type === "Allowed-With-Fee") {
+    if (!dataModelTraining.fee) {
+      throw new BadRequest("Incorrect UDL format: Derivation With Fee must specify fee.");
+    }
+    const fee = dataModelTraining.fee.type + "-" + dataModelTraining.fee.value;
+    derivationValue = dataModelTraining.type + "-" + fee;
+  }
+  return derivationValue;
+}
+
+export const setDerivation = (derivation: Derivation | CommercialUse): string => {
   let derivationValue = derivation.type as string;
   if (derivation.type === "Allowed-With-RevenueShare") {
     if (!derivation.value) {
       throw new BadRequest("Incorrect UDL format: Derivation With Revenue Share must specify % value.");
     }
     derivationValue = derivationValue + "-" + derivation.value + "%";
+  }
+  if (derivation.type === "Allowed-With-Fee") {
+    if (!derivation.fee) {
+      throw new BadRequest("Incorrect UDL format: Derivation With Fee must specify fee.");
+    }
+    const fee = derivation.fee.type + "-" + derivation.fee.value;
+    derivationValue = derivation.type + "-" + fee;
   }
   return derivationValue;
 }
